373e6aa9bbb92d167e19f3e5132577456e6a1a8e
Felipe Brigagão <103532341+felibrisantos@users.noreply.github.com>
2024-02-09 13:04:00 -0300

    Atualizar o new-note-card.tsx

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -18,6 +18,10 @@ export function NewNoteCard({ onNoteCreated }: newNoteCardProps) {
     setShouldShowOnBoarding(false);
   }
 
+  function handleStopEditor() {
+    setShouldShowOnBoarding(true) 
+} 
+
   function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
     setContent(event.target.value);
 
@@ -99,7 +103,7 @@ export function NewNoteCard({ onNoteCreated }: newNoteCardProps) {
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
 
         <Dialog.Content className="fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] outline-none bg-slate-700 md:rounded-md flex flex-col">
-          <Dialog.Close onClick={handleContentChanged} className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+          <Dialog.Close onClick={handleStopEditor} className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="" />
           </Dialog.Close>
 
